11f4b46b98afb75f14973a00318ecf00e4766499
Yiqin Zhao <yiqinzhao@outlook.com>
2020-07-30 20:51:43 -0400

    Initial Commit

---
+++ Shared/YouTuBunMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTuBunMusicApp.swift
+//  Shared
+//
+//  Created by Yiqin Zhao on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct YouTuBunMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
