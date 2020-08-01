046dd72ed99e6c58cedeba28e554d9fa30f0f9ca
Yiqin Zhao <yiqinzhao@outlook.com>
2020-08-01 11:36:29 -0400

    :sparkles: Use storyboard as entry for better competibitliy

--- macOS/YouTuBunMusicApp.swift
+++ macOS/YouTuBunMusicApp.swift
@@ -8,18 +8,32 @@
 import SwiftUI
 import AppKit
 
+@main
 class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     var mainWindow: NSWindow!
     var isFullScreen: Bool = false
     var isFullScreenClose: Bool = false
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        mainWindow = NSApplication.shared.windows[0]
+        let contentView = ContentView()
+        
+        let screenReact = NSScreen.main?.frame
+        
+        mainWindow = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: screenReact!.width * 0.7, height: screenReact!.height * 0.7),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        
+        mainWindow.center()
         
         mainWindow.titleVisibility = .hidden
         
+        mainWindow.contentView = NSHostingView(rootView: contentView)
+        mainWindow.makeKeyAndOrderFront(nil)
+        
         mainWindow.isReleasedWhenClosed = false
         mainWindow.delegate = self
+        
         let button = mainWindow.standardWindowButton(.closeButton)
         button?.target = self
         button?.action = #selector(closeMe(_:))
@@ -46,16 +60,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         isFullScreen = false
         if isFullScreenClose {
             NSApp.hide(nil)
-        }
-    }
-}
-
-@main
-struct YouTuBunMusicApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
         }
     }
 }
